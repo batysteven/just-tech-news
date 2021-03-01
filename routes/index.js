f2d0b1de611242132d803647cfb469ca231e0789
Steven Baty <baty_steven@yahoo.com>
2021-03-01 03:01:01 -0700

    fixed routes not working properly

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
+const userRoutes = require('./api');
 
-router.use('/users', userRoutes);
+router.use('/api', userRoutes);
 
 module.exports = router;
