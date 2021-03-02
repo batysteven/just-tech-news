f9331a98f4ec480cc43d1d880d6b3b931ae982b9
Steven Baty <baty_steven@yahoo.com>
2021-03-02 07:02:49 -0700

    created delete-pos.js/edit-post.js/edit-post.handlebars

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,16 @@
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+    await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          title
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    });
+  
+}
+  
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
