0699ece6522f90261422774042a4e7bf0abae8a7
Steven Baty <baty_steven@yahoo.com>
2021-03-01 23:55:16 -0700

    dl style.css, installed handlebars, created view/layouts folders and main.handlebars/homepage.handlebars, public folder for style.css, updated routes name to controllers, created home-routes.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./api');
 const postRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/api', userRoutes);
 router.use('/api', postRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
