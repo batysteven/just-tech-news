d4f9582cadbb71c84bf25f20a8da283d05786a8f
Steven Baty <baty_steven@yahoo.com>
2021-03-02 07:07:48 -0700

    updated snapshot code, fixed api's

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,16 +1,18 @@
 async function deleteFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    await fetch(`/api/posts/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-          title
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-    });
-  
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'DELETE'
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
 }
-  
+
 document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
