bbe3339d0e3ac79243230c1b6b2b78edebdedbbf
Steven Baty <baty_steven@yahoo.com>
2021-03-02 01:59:09 -0700

    add snapshot code

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const userRoutes = require('./api');
-const postRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
-router.use('/api', userRoutes);
-router.use('/api', postRoutes);
-router.use('/', homeRoutes);
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
